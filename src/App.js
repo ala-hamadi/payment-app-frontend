8a841e9c9ddf5f584f006be2ec726b8a8f1b7567
ala-hamadi <ala.hamadi.01@gmail.com>
2023-11-08 16:27:37 +0100

    feat: integrate search page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -13,6 +14,15 @@ function App() {
           element={
             <Suspense>
               <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          exact
+          path='/search'
+          element={
+            <Suspense>
+              <Search />
             </Suspense>
           }
         />
