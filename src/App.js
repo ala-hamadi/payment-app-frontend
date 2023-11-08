db17900e7ed03f30659a3fcbd70c45c3886f691c
ala-hamadi <ala.hamadi.01@gmail.com>
2023-11-08 16:26:59 +0100

    feat: integrate home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import React, { Suspense } from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          exact
+          path='/'
+          element={
+            <Suspense>
+              <Home />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
