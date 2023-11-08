b4b3dd5c26f4b965bead6141148bb6ab842e786c
ala-hamadi <ala.hamadi.01@gmail.com>
2023-11-08 18:26:39 +0100

    fix: clean and improve payment page and cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import { notify } from "../utils/HelperFunctions";
-
+import productImg from "../assets/images/product.jpg";
 const Header = (props) => {
   const [isOpenMobileMenu, setIsOpenMobileMenu] = useState(false);
   const [isOpenCart, setIsOpenCart] = useState(false);
 
   const info = () => {
-    notify("Not required for testing 😜", toast, "info");
+    notify("Not required for our technical test 😜", toast, "info");
   };
   const openMobileMenu = () => {
     setIsOpenMobileMenu(!isOpenMobileMenu);
@@ -47,7 +47,7 @@ const Header = (props) => {
             <ul className='header-cart-wrapitem w-full'>
               <li className='header-cart-item flex-w flex-t m-b-12'>
                 <div className='header-cart-item-img'>
-                  <img src='images/item-cart-01.jpg' alt='IMG' />
+                  <img src={productImg} alt='IMG' />
                 </div>
                 <div className='header-cart-item-txt p-t-8'>
                   <a
@@ -59,48 +59,20 @@ const Header = (props) => {
                   <span className='header-cart-item-info'>1 x $19.00</span>
                 </div>
               </li>
-              <li className='header-cart-item flex-w flex-t m-b-12'>
-                <div className='header-cart-item-img'>
-                  <img src='images/item-cart-02.jpg' alt='IMG' />
-                </div>
-                <div className='header-cart-item-txt p-t-8'>
-                  <a
-                    href='#'
-                    className='header-cart-item-name m-b-18 hov-cl1 trans-04'
-                  >
-                    Converse All Star
-                  </a>
-                  <span className='header-cart-item-info'>1 x $39.00</span>
-                </div>
-              </li>
-              <li className='header-cart-item flex-w flex-t m-b-12'>
-                <div className='header-cart-item-img'>
-                  <img src='images/item-cart-03.jpg' alt='IMG' />
-                </div>
-                <div className='header-cart-item-txt p-t-8'>
-                  <a
-                    href='#'
-                    className='header-cart-item-name m-b-18 hov-cl1 trans-04'
-                  >
-                    Nixon Porter Leather
-                  </a>
-                  <span className='header-cart-item-info'>1 x $17.00</span>
-                </div>
-              </li>
             </ul>
             <div className='w-full'>
               <div className='header-cart-total w-full p-tb-40'>
                 Total: $75.00
               </div>
               <div className='header-cart-buttons flex-w w-full'>
-                <a
-                  href='shoping-cart.html'
+                <Link
+                  to='/payment'
                   className='flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10'
                 >
                   View Cart
-                </a>
+                </Link>
                 <a
-                  href='shoping-cart.html'
+                  href='#'
                   className='flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-b-10'
                 >
                   Check Out
